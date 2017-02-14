328509702cb8b392ea5975f6b05f9aad828c01d0
BRD-E <BRD-E@outlook.com>
2017-02-14 15:35:22 -0700

    Added ability to encrypt strings

--- RSA.java
+++ RSA.java
@@ -9,50 +9,53 @@ class RSA{
 	private static BigInteger prime2;
 	private static BigInteger modulus;
 	private static BigInteger totient;
-	private static BigInteger publicKey;
+	private static int fixedPublicKey = 65537;
+	private static BigInteger publicKey = BigInteger.valueOf(fixedPublicKey);
 	private static BigInteger privateKey;
-	private static Random rnd = new Random();
+	private static Random rand = new Random();
 	private static String message;
 
-
-    //*************************************************************************
-    // Main
     public static void main(String[] args) throws IOException {
         generateKeys();
     }
+
 	static void generateKeys()
 	{
+		// This is the basic outline for RSA encryption. Found on Wiki
 		generatePrime12();
 		generateModulus();
 		generateTotient();
 		generatePublicKey();
 		generatePrivateKey();
 		printKeys();
-		decryptMessage(encryptMessage(BigInteger.valueOf(2048)));
+		System.out.println(toText(decryptMessage(encryptMessage(toBigInteger("test")))));
+
 	}
+
 	static void generatePrime12()
 	{
-		//prime1 = BigInteger.probablePrime(PRIME_SIZE,rnd);
-		//prime2 = BigInteger.probablePrime(PRIME_SIZE,rnd);
-		prime1 = BigInteger.valueOf(61);
-		prime2 = BigInteger.valueOf(53);
+		prime1 = BigInteger.probablePrime(PRIME_SIZE,rand);
+		prime2 = BigInteger.probablePrime(PRIME_SIZE,rand);
 	}
+
 	static void generateModulus()
 	{
 		modulus = prime1.multiply(prime2);
 	}
+
 	static void generateTotient()
 	{
-		// |(p-1) * (q-1) | / gcd((p-1), (q-1))
+		// |(p-1) * (q-1) | / gcd((p-1), (q-1)) or leastCommonMultiple(p-1, q-1)
 		BigInteger prime1Minus1 = prime1.subtract(BigInteger.ONE);
 		BigInteger prime2Minus1 = prime2.subtract(BigInteger.ONE);
-		BigInteger multiplied = prime1Minus1.multiply(prime2Minus1);
-		BigInteger gcd = prime1Minus1.gcd(prime2Minus1);
-		totient = multiplied.divide(gcd);
+		BigInteger numerator = prime1Minus1.multiply(prime2Minus1);
+		BigInteger greatestCommonDenominator = prime1Minus1.gcd(prime2Minus1);
+		totient = numerator.divide(greatestCommonDenominator);
 	}
+
 	static void generatePublicKey()
 	{
-		publicKey = BigInteger.valueOf(16);
+		publicKey = publicKey.subtract(BigInteger.ONE); // This will make the fixedPublicKey the publicKey if possible. if not it will find a new value.
         while(true)
 		{
             publicKey = publicKey.add(BigInteger.ONE);
@@ -69,22 +72,37 @@ class RSA{
 	{
 		privateKey = publicKey.modInverse(totient);
 	}
+
 	static void printKeys()
 	{
-		System.out.println("Modulus: " + modulus.toString());
+		System.out.println("modulus: " + modulus.toString());
 		System.out.println("publicKey: " + publicKey.toString());
 		System.out.println("privateKey: " + privateKey.toString());
 		System.out.println("totient: " + totient.toString());
 	}
+
 	static BigInteger encryptMessage(BigInteger message)
 	{
-		BigInteger encrypted = message.modPow(publicKey, modulus);
-		System.out.println("encrypted: " + encrypted.toString());
-		return encrypted;
+		BigInteger encryptedMessage = message.modPow(publicKey, modulus);
+		return encryptedMessage;
 	}
-	static void decryptMessage(BigInteger message)
+
+	static BigInteger decryptMessage(BigInteger message)
 	{
-		BigInteger decrypted = message.modPow(privateKey, modulus);
-		System.out.println("decrypted: " + decrypted.toString());
+		BigInteger decryptedMessage = message.modPow(privateKey, modulus);
+		return decryptedMessage;
+	}
+
+	static BigInteger toBigInteger(String message)
+	{
+		byte[] byteString = message.getBytes();
+		return new BigInteger(byteString);
+	}
+
+	static String toText(BigInteger text)
+	{
+		System.out.println(text.toString());
+		byte[] byteArray = text.toByteArray();
+		return new String(byteArray);
 	}
 }
